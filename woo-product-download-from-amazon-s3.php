daf8d9b68e383c9e33cedd2c2969911ae20ebcaa
Emran Ahmed <emran.bd.08@gmail.com>
2016-07-30 13:14:24 +0600

    File Renamed

--- woo-product-download-from-amazon-s3.php
+++ woo-product-download-from-amazon-s3.php
@@ -1,17 +1,16 @@
 <?php
 	/**
 	 * Plugin Name:       Woo Product Download from Amazon S3
-	 * Description:       Woocommerce Product Download / Upload to / from using Amazon S3 service.
+	 * Description:       WooCommerce Product Download / Upload to / from using Amazon S3 service.
 	 * Version:           1.0.0
 	 * Author:            Emran
 	 * Author URI:        https://emran.me/
 	 * License:           GPLv2.0+
 	 * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
-	 * Text Domain:       ea-wc-amazon-s3
+	 * Text Domain:       woo-product-download-from-amazon-s3
 	 */
 
 	defined( 'ABSPATH' ) or die( 'Keep Quit' );
-
 
 	if ( ! class_exists( 'EA_WC_Amazon_S3' ) ):
 
@@ -43,7 +42,7 @@
 			}
 
 			public function init() {
-				load_plugin_textdomain( 'ea-wc-amazon-s3', FALSE, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+				load_plugin_textdomain( 'woo-product-download-from-amazon-s3', FALSE, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 			}
 
 			private function hooks() {
@@ -58,10 +57,10 @@
 				add_filter( 'woocommerce_settings_tabs_array', array( $this, 'add_tab' ), 50 );
 
 				// Show Settings Fields
-				add_action( 'woocommerce_settings_tabs_ea-wc-amazon-s3', array( $this, 'settings_fields' ) );
+				add_action( 'woocommerce_settings_tabs_woo-product-download-from-amazon-s3', array( $this, 'settings_fields' ) );
 
 				// Save Settings Fields
-				add_action( 'woocommerce_update_options_ea-wc-amazon-s3', array( $this, 'update_settings' ) );
+				add_action( 'woocommerce_update_options_woo-product-download-from-amazon-s3', array( $this, 'update_settings' ) );
 
 				// File Download Process
 				add_action( 'woocommerce_download_file_from_ea_wc_amazon_s3', array( $this, 'process_download' ), 10, 2 );
@@ -90,11 +89,11 @@
 				}
 
 				if ( ! isset( $_POST[ '_wpnonce' ] ) || ! wp_verify_nonce( $_POST[ '_wpnonce' ], 'ea_wc_amazon_s3_upload_file' ) ) {
-					wp_die( esc_html__( 'Not Verified', 'ea-wc-amazon-s3' ), esc_html__( 'Error', 'ea-wc-amazon-s3' ), array( 'back_link' => TRUE ) );
+					wp_die( esc_html__( 'Not Verified', 'woo-product-download-from-amazon-s3' ), esc_html__( 'Error', 'woo-product-download-from-amazon-s3' ), array( 'back_link' => TRUE ) );
 				}
 
 				if ( empty( $_FILES[ 'ea_wc_amazon_s3_file' ] ) || empty( $_FILES[ 'ea_wc_amazon_s3_file' ][ 'name' ] ) ) {
-					wp_die( esc_html__( 'Please select a file to upload', 'ea-wc-amazon-s3' ), esc_html__( 'Error', 'ea-wc-amazon-s3' ), array( 'back_link' => TRUE ) );
+					wp_die( esc_html__( 'Please select a file to upload', 'woo-product-download-from-amazon-s3' ), esc_html__( 'Error', 'woo-product-download-from-amazon-s3' ), array( 'back_link' => TRUE ) );
 				}
 
 				$file = array(
@@ -118,7 +117,7 @@
 					delete_transient( 'ea_wc_amazon_s3_file_name' );
 					delete_transient( 'ea_wc_amazon_s3_bucket_name' );
 
-					wp_die( esc_html__( 'Something went wrong during the upload process.', 'ea-wc-amazon-s3' ), esc_html__( 'Error', 'ea-wc-amazon-s3' ), array( 'back_link' => TRUE ) );
+					wp_die( esc_html__( 'Something went wrong during the upload process.', 'woo-product-download-from-amazon-s3' ), esc_html__( 'Error', 'woo-product-download-from-amazon-s3' ), array( 'back_link' => TRUE ) );
 				}
 			}
 
@@ -153,8 +152,8 @@
 					$action_links = apply_filters( 'ea_wc_amazon_s3_action_links', array(
 						'settings' => sprintf( '<a href="%1$s" title="%2$s">%2$s</a>', esc_url( add_query_arg( array(
 							                                                                                       'page' => 'wc-settings',
-							                                                                                       'tab'  => 'ea-wc-amazon-s3'
-						                                                                                       ), admin_url( 'admin.php' ) ) ), esc_attr__( 'Amazon S3 Settings', 'ea-wc-amazon-s3' ) ),
+							                                                                                       'tab'  => 'woo-product-download-from-amazon-s3'
+						                                                                                       ), admin_url( 'admin.php' ) ) ), esc_attr__( 'Amazon S3 Settings', 'woo-product-download-from-amazon-s3' ) ),
 					) );
 
 					return array_merge( $action_links, $links );
@@ -170,7 +169,7 @@
 
 			public function admin_enqueue_scripts() {
 				wp_enqueue_media();
-				wp_enqueue_script( 'ea-wc-amazon-s3', untrailingslashit( plugin_dir_url( __FILE__ ) ) . '/js/scripts.js', array( 'jquery' ), FALSE, TRUE );
+				wp_enqueue_script( 'woo-product-download-from-amazon-s3', untrailingslashit( plugin_dir_url( __FILE__ ) ) . '/js/scripts.js', array( 'jquery' ), FALSE, TRUE );
 			}
 
 			public function buckets_iframe() {
@@ -211,22 +210,22 @@
 				<div style="margin: 20px 1em 1em; padding-right:20px;" id="media-items">
 					<?php
 						if ( ! $bucket ) { ?>
-							<h3 class="media-title"><?php esc_html_e( 'Select a Bucket', 'ea-wc-amazon-s3' ); ?></h3>
+							<h3 class="media-title"><?php esc_html_e( 'Select a Bucket', 'woo-product-download-from-amazon-s3' ); ?></h3>
 							<?php
 
 							if ( is_array( $buckets ) ) {
 
 								echo '<table class="wp-list-table widefat fixed striped" style="max-height: 500px;overflow-y:scroll;">';
 								echo '<tr>';
-								echo '<th>' . esc_html__( 'Bucket name', 'ea-wc-amazon-s3' ) . '</th>';
-								echo '<th>' . esc_html__( 'Actions', 'ea-wc-amazon-s3' ) . ' </th>';
+								echo '<th>' . esc_html__( 'Bucket name', 'woo-product-download-from-amazon-s3' ) . '</th>';
+								echo '<th>' . esc_html__( 'Actions', 'woo-product-download-from-amazon-s3' ) . ' </th>';
 								echo '</tr>';
 
 								foreach ( $buckets as $key => $bucket ) {
 									echo '<tr>';
 									echo '<td>' . $bucket . '</td>';
 									echo '<td>';
-									echo '<a href = "' . esc_url( add_query_arg( 'bucket', $bucket ) ) . '">' . esc_html__( 'Browse', 'ea-wc-amazon-s3' ) . '</a>';
+									echo '<a href = "' . esc_url( add_query_arg( 'bucket', $bucket ) ) . '">' . esc_html__( 'Browse', 'woo-product-download-from-amazon-s3' ) . '</a>';
 									echo '</td>';
 									echo '</tr>';
 								}
@@ -241,15 +240,15 @@
 								$i           = 0;
 								$total_items = count( $files );
 
-								echo '<p><button class="button-secondary" onclick="history.back();">' . esc_html__( 'Go Back', 'ea-wc-amazon-s3' ) . '</button></p>';
+								echo '<p><button class="button-secondary" onclick="history.back();">' . esc_html__( 'Go Back', 'woo-product-download-from-amazon-s3' ) . '</button></p>';
 
 								echo '<table class="wp-list-table widefat fixed striped" style="max-height: 500px;overflow-y:scroll;">';
 
 								echo '<tr>';
-								echo '<th>' . esc_html__( 'File name', 'ea-wc-amazon-s3' ) . '</th>';
-								echo '<th>' . esc_html__( 'File size', 'ea-wc-amazon-s3' ) . '</th>';
-								echo '<th>' . esc_html__( 'Uploaded on', 'ea-wc-amazon-s3' ) . '</th>';
-								echo '<th>' . esc_html__( 'Actions', 'ea-wc-amazon-s3' ) . '</th>';
+								echo '<th>' . esc_html__( 'File name', 'woo-product-download-from-amazon-s3' ) . '</th>';
+								echo '<th>' . esc_html__( 'File size', 'woo-product-download-from-amazon-s3' ) . '</th>';
+								echo '<th>' . esc_html__( 'Uploaded on', 'woo-product-download-from-amazon-s3' ) . '</th>';
+								echo '<th>' . esc_html__( 'Actions', 'woo-product-download-from-amazon-s3' ) . '</th>';
 								echo '</tr>';
 
 								foreach ( $files as $key => $file ) {
@@ -269,10 +268,10 @@
 
 									echo '<td>' . esc_html( $file[ 'name' ] ) . '</td>';
 									echo '<td>' . size_format( $file[ 'size' ] ) . '</td>';
-									echo '<td>' . human_time_diff( $file[ 'time' ] ) . ' ' . esc_html__( 'ago', 'ea-wc-amazon-s3' ) . '</td>';
+									echo '<td>' . human_time_diff( $file[ 'time' ] ) . ' ' . esc_html__( 'ago', 'woo-product-download-from-amazon-s3' ) . '</td>';
 
 									echo '<td>';
-									echo '<a class="insert-s3 button-secondary" href="#" data-s3="' . esc_attr( $file[ 'name' ] ) . '">' . esc_html__( 'Use File', 'ea-wc-amazon-s3' ) . '</a>';
+									echo '<a class="insert-s3 button-secondary" href="#" data-s3="' . esc_attr( $file[ 'name' ] ) . '">' . esc_html__( 'Use File', 'woo-product-download-from-amazon-s3' ) . '</a>';
 									echo '</td>';
 									echo '</tr>';
 									$i ++;
@@ -289,13 +288,13 @@
 							echo '<div class="s3-pagination tablenav">';
 							echo '<div class="tablenav-pages alignright">';
 							if ( isset( $_GET[ 'p' ] ) && $_GET[ 'p' ] > 1 ) {
-								echo '<a class="page-numbers prev button-secondary" href="' . esc_url( remove_query_arg( 'p', $base ) ) . '">' . esc_html__( 'Start Over', 'ea-wc-amazon-s3' ) . '</a>';
+								echo '<a class="page-numbers prev button-secondary" href="' . esc_url( remove_query_arg( 'p', $base ) ) . '">' . esc_html__( 'Start Over', 'woo-product-download-from-amazon-s3' ) . '</a>';
 							}
 							if ( $i >= 10 ) {
 								echo '<a class="page-numbers next button-secondary" href="' . esc_url( add_query_arg( array(
 									                                                                                      'p'     => $page + 1,
 									                                                                                      'start' => $last_file
-								                                                                                      ), $base ) ) . '">' . esc_html__( 'View More', 'ea-wc-amazon-s3' ) . '</a>';
+								                                                                                      ), $base ) ) . '">' . esc_html__( 'View More', 'woo-product-download-from-amazon-s3' ) . '</a>';
 							}
 							echo '</div>';
 							echo '</div>';
@@ -320,8 +319,8 @@
 			}
 
 			public function media_tabs( $tabs ) {
-				$tabs[ 'ea_wc_amazon_buckets' ] = esc_html__( 'Browse S3 Buckets', 'ea-wc-amazon-s3' );
-				$tabs[ 'ea_wc_amazon_upload' ]  = esc_html__( 'Upload to Amazon S3', 'ea-wc-amazon-s3' );
+				$tabs[ 'ea_wc_amazon_buckets' ] = esc_html__( 'Browse S3 Buckets', 'woo-product-download-from-amazon-s3' );
+				$tabs[ 'ea_wc_amazon_upload' ]  = esc_html__( 'Upload to Amazon S3', 'woo-product-download-from-amazon-s3' );
 
 				return $tabs;
 			}
@@ -354,7 +353,7 @@
 				</style>
 				<script>
 					jQuery(document).ready(function ($) {
-						$('.ea-wc-amazon-s3-insert').on('click', function (e) {
+						$('.woo-product-download-from-amazon-s3-insert').on('click', function (e) {
 							e.preventDefault();
 
 							var link = "<?php echo get_transient( 'ea_wc_amazon_s3_abs_path' ) ?>";
@@ -375,7 +374,7 @@
 									<option value="<?php echo esc_attr( $bucket ); ?>"><?php echo esc_html( $bucket ); ?></option>
 								<?php endforeach; ?>
 							</select>
-							<label for="ea_wc_amazon_s3_bucket"><?php esc_html_e( 'Select a bucket to upload the file to', 'ea-wc-amazon-s3' ); ?></label>
+							<label for="ea_wc_amazon_s3_bucket"><?php esc_html_e( 'Select a bucket to upload the file to', 'woo-product-download-from-amazon-s3' ); ?></label>
 						</p>
 						<p>
 							<input type="file" name="ea_wc_amazon_s3_file">
@@ -383,12 +382,12 @@
 
 						<p>
 							<input type="submit" name="ea_wc_amazon_s3_upload_submit" class="button-secondary"
-							       value="<?php esc_attr_e( 'Upload to S3', 'ea-wc-amazon-s3' ); ?>"/>
+							       value="<?php esc_attr_e( 'Upload to S3', 'woo-product-download-from-amazon-s3' ); ?>"/>
 						</p>
 						<?php wp_nonce_field( 'ea_wc_amazon_s3_upload_file' ); ?>
 						<?php
 							if ( ! empty( $_GET[ 'ea_wc_amazon_s3_success' ] ) && '1' == $_GET[ 'ea_wc_amazon_s3_success' ] ) {
-								echo '<div class="ea_wc_amazon_s3_errors"><p class="ea_wc_amazon_s3_success">' . __( 'Success! <a href="#" class="ea-wc-amazon-s3-insert">Insert uploaded file</a>.' ) . '</p></div>';
+								echo '<div class="ea_wc_amazon_s3_errors"><p class="ea_wc_amazon_s3_success">' . __( 'Success! <a href="#" class="woo-product-download-from-amazon-s3-insert">Insert uploaded file</a>.' ) . '</p></div>';
 							}
 						?>
 					</form>
@@ -535,7 +534,7 @@
 			}
 
 			public function add_tab( $settings_tabs ) {
-				$settings_tabs[ 'ea-wc-amazon-s3' ] = esc_html__( 'Amazon S3 Settings', 'ea-wc-amazon-s3' );
+				$settings_tabs[ 'woo-product-download-from-amazon-s3' ] = esc_html__( 'Amazon S3 Settings', 'woo-product-download-from-amazon-s3' );
 
 				return $settings_tabs;
 			}
@@ -549,27 +548,27 @@
 				$settings = array();
 
 				$settings[] = array(
-					'title' => esc_html__( 'Amazon S3 Settings', 'ea-wc-amazon-s3' ),
+					'title' => esc_html__( 'Amazon S3 Settings', 'woo-product-download-from-amazon-s3' ),
 					'type'  => 'title',
 					'id'    => 'ea_wc_amazon_s3',
 				);
 
 				$settings[] = array(
-					'name' => esc_html__( 'Amazon S3 Access Key ID', 'ea-wc-amazon-s3' ),
-					'desc' => esc_html__( 'After logging into your Amazon S3 account, click on "Security Credentials" in the sidebar. Scroll down to "Access Keys (Access Key ID and Secret Access Key)" and you will see your Access Key ID. Copy and paste it here.', 'ea-wc-amazon-s3' ),
+					'name' => esc_html__( 'Amazon S3 Access Key ID', 'woo-product-download-from-amazon-s3' ),
+					'desc' => esc_html__( 'After logging into your Amazon S3 account, click on "Security Credentials" in the sidebar. Scroll down to "Access Keys (Access Key ID and Secret Access Key)" and you will see your Access Key ID. Copy and paste it here.', 'woo-product-download-from-amazon-s3' ),
 					'id'   => 'ea_wc_amazon_s3_key',
 					'type' => 'text',
 				);
 
 				$settings[] = array(
-					'name' => esc_html__( 'Amazon S3 Secret Key', 'ea-wc-amazon-s3' ),
-					'desc' => esc_html__( 'In the same Access Credentials area, your "Secret Key" will be hidden. You will need to click the "Show" link to see it. Copy and paste it here.', 'ea-wc-amazon-s3' ),
+					'name' => esc_html__( 'Amazon S3 Secret Key', 'woo-product-download-from-amazon-s3' ),
+					'desc' => esc_html__( 'In the same Access Credentials area, your "Secret Key" will be hidden. You will need to click the "Show" link to see it. Copy and paste it here.', 'woo-product-download-from-amazon-s3' ),
 					'id'   => 'ea_wc_amazon_s3_secret_key',
 					'type' => 'text',
 				);
 
 				$settings[] = array(
-					'name' => esc_html__( 'Amazon S3 EndPoint', 'ea-wc-amazon-s3' ),
+					'name' => esc_html__( 'Amazon S3 EndPoint', 'woo-product-download-from-amazon-s3' ),
 					'desc' => __( ' Amazon S3 Endpoint like: <code>s3-us-west-2.amazonaws.com</code>' ),
 					'id'   => 'ea_wc_amazon_s3_endpoint',
 					'type' => 'text',
